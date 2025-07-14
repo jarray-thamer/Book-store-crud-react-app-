58780fdf147567ba1dc314693bee5a1ab33e314b
Thamer Jarray <jarray.thamerr@gmail.com>
2025-07-14 15:45:17 +0100

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("http://localhost:5555/books")
+      .get(`${import.meta.env.vite.BACKEND_URL}`)
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
