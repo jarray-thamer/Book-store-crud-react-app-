9c5a921ac115b198867acaa8e0a00c4e4c114696
Thamer Jarray <jarray.thamerr@gmail.com>
2025-07-14 15:43:57 +0100

    Update CreateBook.jsx

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -23,7 +23,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post("http://localhost:5555/books", data)
+      .post(`${import.meta.env.vite.BACKEND_URL}/books`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Book Created successfully", { variant: "success" });
